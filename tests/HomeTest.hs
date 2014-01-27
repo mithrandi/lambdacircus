6912acc33026884330a832ceb6e118d5ae5c3492
Tristan Seligmann <mithrandi@mithrandi.net>
2014-01-27 18:32:29 +0200

    Attempt to fix the tests.

---
+++ tests/HomeTest.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+module HomeTest
+    ( homeSpecs
+    ) where
+
+import TestImport
+import qualified Data.List as L
+
+homeSpecs :: Spec
+homeSpecs =
+    ydescribe "These are some example tests" $ do
+        return ()
