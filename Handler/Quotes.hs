994582011a9080efabacbfecd26addc8d10dd312
Tristan Seligmann <mithrandi@mithrandi.net>
2017-06-11 22:28:13 +0200

    No english on Google Cloud SQL :(

--- Handler/Quotes.hs
+++ Handler/Quotes.hs
@@ -41,7 +41,7 @@ getDefQuotesR = do
    Just matches -> do
      quotes <- runDB $ rawSql q [toPersistValue matches]
      renderQuotes quotes Nothing Nothing
-  where q = "SELECT ?? FROM quote, plainto_tsquery('english', ?) query WHERE to_tsvector(translate(content, '<>', '  ')) @@ query ORDER BY ts_rank_cd(to_tsvector(translate(content, '<>', '  ')), query) DESC LIMIT 50"
+  where q = "SELECT ?? FROM quote, plainto_tsquery('simple', ?) query WHERE to_tsvector(translate(content, '<>', '  ')) @@ query ORDER BY ts_rank_cd(to_tsvector(translate(content, '<>', '  ')), query) DESC LIMIT 50"
 
 
 getQuotesR :: Integer -> Handler TypedContent
